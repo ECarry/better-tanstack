3cf66f04e52a7379362b2259ddc0ec58c9035419
ecarry <lianshiliang93@gmail.com>
2025-12-07 13:59:43 +0000

    chore: remove all project files to prepare for fresh start
    Delete entire TanStack project including configuration, source code, dependencies, and documentation. Remove .cta.json, components.json, drizzle config, eslint config, package.json, README, and all other project files to reset repository.

--- src/integrations/trpc/router.ts
+++
@@ -1,7 +0,0 @@
-import { todosRouter } from '@/modules/todos/server/procedures'
-import { createTRPCRouter } from './init'
-
-export const trpcRouter = createTRPCRouter({
-  todos: todosRouter,
-})
-export type TRPCRouter = typeof trpcRouter
